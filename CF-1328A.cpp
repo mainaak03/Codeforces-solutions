c6a136bdf6d0d603f7a5244460eddfb60ea07b97
mainaak03 <91618236+mainaak03@users.noreply.github.com>
2021-10-25 19:14:14 +0530

    Add files via upload

---
+++ CF-1328A.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int  main() {
+
+int n;
+cin>>n;
+int arr[n][2];
+for (int i=0; i<n; i++) {cin >>arr[i][0] >>arr[i][1] ;}
+
+for (int i=0; i<n; i++) {
+if (arr[i][0] % arr[i][1] == 0) {cout<<0 <<endl;}
+else {cout <<arr[i][1] - (arr[i][0] % arr[i][1]) <<endl ;}
+}
+
+return 0;
+}
---
+++ CF-546A.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+int k, n, w;
+cin >>k >>n >>w ;
+int cost=0;
+for (int i=0; i<=w; i++) {
+    cost += i*k;
+}
+int res;
+res = cost - n ;
+if (res<0) {cout<<"0";}
+else {cout<<res;}
+return 0;
+}
