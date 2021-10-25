fe88259a0e9c65fdb7f1b536898ccdf593bae3e8
mainaak03 <91618236+mainaak03@users.noreply.github.com>
2021-10-25 20:03:22 +0530

    Add files via upload

---
+++ CF-1335A.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    long int t, n;
+    cin>>t;
+    for (int i=1; i<=t; i++) {
+    n=0;
+    cin>>n;
+    if (n<=2) {cout<<"0" <<endl;}
+    else {cout<<(n-1)/2 <<endl;}
+}
+return 0;
+}
