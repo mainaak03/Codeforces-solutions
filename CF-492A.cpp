767e98bc455e2527155662189489a1035c4af4dd
Mainaak Chakraborty <91618236+mainaak03@users.noreply.github.com>
2021-10-26 21:35:31 +0530

    Add files via upload

---
+++ CF-492A.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+long int sum(int l) {
+    long int sum = ((l*(l+1)*(2*l +1))/6 + (l*(l+1))/2 )/2 ;
+    return sum;
+}
+
+int main() {
+
+int l=1;
+int n;
+int res;
+cin>>n;
+if (n==1) {res=1;}
+else {
+while (sum(l)<=n) {res=l; l++;}
+}
+cout <<res;
+return 0;
+}
