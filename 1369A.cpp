f242d3b0482815bb940fb92edef553d1b3cc16a6
Mainaak Chakraborty <91618236+mainaak03@users.noreply.github.com>
2021-10-28 11:18:25 +0530

    Create 1369A.cpp

---
+++ 1369A.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+ 
+int t,n;
+cin >>t;
+for (int i=0; i<t; i++) {
+    cin >>n;
+    if (n>=4 && n%4==0) {cout <<"YES" <<endl;}
+    else {cout <<"NO" <<endl;}
+}
+ 
+return 0;
+}
